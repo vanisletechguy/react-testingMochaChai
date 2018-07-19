31088065510e5b090a7ca07a571a4827f629a4b3
mikenic <temperanceaubie@gmail.com>
2018-07-18 22:38:30 -0700

    react testing demo

---
+++ test/components/comment_list_test.js
@@ -0,0 +1,20 @@
+import { renderComponent, expect } from '../test_helper';
+import CommentList from '../../src/components/comment_list';
+
+describe('CommentList', () => {
+	let component;
+
+	beforeEach(() => {
+		const props = {comments: ['new comment', 'comment two']};
+		component = renderComponent(CommentList, null, props);
+	});
+
+	it('shows an LI for each comment', () => {
+		expect(component.find('li').length).to.equal(2);	
+	});
+
+	it('shows each comment that is provided', () => {
+		expect(component).to.contain('new comment');
+		expect(component).to.contain('comment two');
+	});
+});
